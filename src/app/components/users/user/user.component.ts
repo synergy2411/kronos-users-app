11373e1160de82a5d70fdb668b8e99e01fe61ec7
Sumit Khandelwal <sumitgniit@gmail.com>
2021-06-23 11:04:15 +0530

    lifecycle added

--- src/app/components/users/user/user.component.ts
+++ src/app/components/users/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { User } from 'src/app/model/user.model';
 
 
 @Component({
@@ -6,13 +7,43 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent
+implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked, OnDestroy {
 
-  @Input() user : any;
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges", changes);
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+  }
+
+
+
+  @Input() user : User;
+
   @Output() childEvent = new EventEmitter<string>()
 
   onClick(){
-    this.childEvent.emit("Event Data")
+    this.childEvent.emit("Event Data");
   }
 
 }
